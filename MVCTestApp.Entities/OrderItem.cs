a2e9a0e1e6fcc1d1a9595e67e546425ccf0c23f9
sh4rdu1 <me.shardul.sp@gmail.com>
2023-08-31 22:17:12 -0400

    Added OrderItem entity
    Added OrderItem entity

---
+++ MVCTestApp.Entities/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCTestApp.Entities
+{
+    internal class OrderItem
+    {
+        public int Id { get; set; }
+
+        public string OrderName { get; set; }
+
+        public int OrderId { get; set; }
+        [ForeignKey(OrderName)]
+
+    }
+}
